784d48f6bf7c88453a02859f5364fbd4f6d5141c
SlViKo <41319845+SlViKo@users.noreply.github.com>
2020-07-24 11:35:59 +0300

    репазиторий для алгоримов

---
+++ src/lesson2/Main.java
@@ -0,0 +1,19 @@
+package lesson2;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MyArrayList<Integer> msal = new MyArrayList<>(1_000_000);
+
+
+    }
+
+    public static void fillArrayInt(MyArrayList<Integer> msal) {
+        Random rnd = new Random(1000);
+        for (int i = 0; i < msal.size(); i++) {
+            
+        }
+    }
+}
